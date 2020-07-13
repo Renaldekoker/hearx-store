5cbcbecc5f7b407b68a99f1a6f666391b4f7ae4e
Renaldekoker <renaldovd@gmail.com>
2020-07-13 03:24:48 +0200

    Sorting functionality added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatSelectModule } from '@angular/material/select';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,9 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
     MatGridListModule,
     MatButtonModule,
     MatIconModule,
-    MatProgressBarModule
+    MatProgressBarModule,
+    MatSelectModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -4,6 +4,8 @@ import {IProduct} from '../product/state/product.model';
 import {ProductQuery} from '../product/state/product.query';
 import {ProductService} from '../product/state/product.service';
 import {FormControl} from '@angular/forms';
+import {startWith, switchMap} from 'rxjs/internal/operators';
+import {Order} from '@datorama/akita';
 
 @Component({
   selector: 'app-landing-page',
@@ -12,8 +14,9 @@ import {FormControl} from '@angular/forms';
 })
 export class LandingPageComponent implements OnInit {
   products$ = this.productQuery.selectAll();
-  selectLoading$ = this.productQuery.selectLoading();
-  sortBy = new FormControl();
+  isLoading$ = this.productQuery.selectLoading();
+  sortBy = new FormControl('');
+  sortOrder = new FormControl('asc');
 
   constructor(
     private productQuery: ProductQuery,
@@ -30,4 +33,7 @@ export class LandingPageComponent implements OnInit {
     this.productService.get();
   }
 
+  sort() {
+    this.products$ = this.productQuery.selectAll({ sortBy: this.sortBy.value, sortByOrder: this.sortOrder.value});
+  }
 }
